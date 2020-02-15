2218a57b164325d0ae13dcb5598b9dcb87f725be
Luis Peixoto <lmpeixoto@gmail.com>
2020-02-15 07:52:24 +0000

    tasks routers

--- src/routers/task.js
+++ src/routers/task.js
@@ -8,7 +8,7 @@ router.post('/tasks', auth, async (req, res) => {
         ...req.body,
         owner: req.user._id
     })
-    
+
     try {
         await task.save()
         res.status(201).send(task)
@@ -19,23 +19,26 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({ owner: req.user._id })
-        res.send(tasks)
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
-        res.status(500).send(e)
+        res.status(500).send()
     }
 })
 
 router.get('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id
+
     try {
-        const _id = req.params.id
         const task = await Task.findOne({ _id, owner: req.user._id })
+
         if (!task) {
             return res.status(404).send()
         }
+
         res.send(task)
     } catch (e) {
-        res.status(500).send(e)
+        res.status(500).send()
     }
 })
 
@@ -43,11 +46,13 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!' })
     }
+
     try {
-        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
 
         if (!task) {
             return res.status(404).send()
@@ -64,13 +69,14 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 router.delete('/tasks/:id', auth, async (req, res) => {
     try {
         const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
 
+        if (!task) {
+            res.status(404).send()
+        }
+
+        res.send(task)
     } catch (e) {
-        res.status(500).send(e)
+        res.status(500).send()
     }
 })
 
