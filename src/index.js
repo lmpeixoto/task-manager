65ec7e1231841e7075ae4fbb9ed3814845370172
Luis Peixoto <lmpeixoto@gmail.com>
2020-02-15 07:48:27 +0000

    before changing files

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
+// })
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+// const Task = require('./models/task')
+// const User = require('./models/user')
+
+// const main = async () => {
+//     // const task = await Task.findById('5e45c7a7a00b0546a00e674b')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
+
+//     const user = await User.findById('5e45c7229b5fc1330cdbd431')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+
+// }
+
+// main()
